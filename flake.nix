bb959697d0e5948c8c87b1a2a8cfa4170a7642ae
t8y2 <1156263951@qq.com>
2026-07-08 10:02:45 +0800

    feat(release): bump app to 0.5.50

--- flake.nix
+++ flake.nix
@@ -165,7 +165,7 @@
         # ------------------------------------------------------------------ #
         packages.dbx-desktop = pkgs.stdenv.mkDerivation (finalAttrs: {
           pname = "dbx-desktop";
-          version = "0.5.49";
+          version = "0.5.50";
 
           src = pkgs.lib.cleanSource ./.;
 
